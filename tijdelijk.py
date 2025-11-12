92f19da6c19c9a447ede2b691a5716ee5c16fe70
niconha <nico.de.keijne@gmail.com>
2025-11-12 10:46:23 +0100

    functie toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,28 @@
-prijzen = {"aardbei": 3,
-                   "vanille": 4,
-                   "chocolade": 5}
+from helper import decoreer
 
-aanbieding = prijzen["aardbei"] * 0.8 
+def print_aanbieding():
+   prijzen = {"aardbei": 3,
+            "vanille": 4,
+            "chocolade": 5}
 
-reclame_tekst = f"vandaag in de aanbieding: vanille-ijs 1 liter - slechts € {aanbieding} "
+   aanbieding = prijzen ["aardbei"] * 0.8 
 
-reclame_tekst2 = reclame_tekst [:62] 
+   reclame_tekst = f"vandaag in de aanbieding: vanille-ijs 1 liter - slechts € {aanbieding} "
 
-reclame_tekst3 = reclame_tekst2.upper()
+   reclame_tekst2 = reclame_tekst [:62] 
 
-reclame_tekst4 = reclame_tekst3.split()
+   reclame_tekst3 = reclame_tekst2.upper()
 
-for el in reclame_tekst4:
-    if len (el) > 4:
-       print (el.upper())
-    else:
-       print (el.lower())
+   reclame_tekst4 = reclame_tekst3.split()
+
+   for el in reclame_tekst4:
+      if len (el) > 4:
+         print (el.upper())
+      else:
+         print (el.lower())
+
+
+decoreer ("aanbieding")
+print_aanbieding ()
+
+# Na def de HELE code laten inspringen met TAB, en het is print_aanbieding () en niet print (print_aanbieding)
